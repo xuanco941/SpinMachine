760a72b6c8755c6226750291858dc1bb177b2055
xuanco941 <xuanco941@gmail.com>
2023-12-14 15:57:28 +0700

    random result

--- assets/Scripts/board1Controller.ts
+++ assets/Scripts/board1Controller.ts
@@ -7,9 +7,9 @@ export class board1Controller extends Component {
     @property({ type: Prefab })
     public reelPrefab: Prefab | null = null;
 
-    public reel1Script:reelController;
-    public reel2Script:reelController;
-    public reel3Script:reelController;
+    public reel1Script: reelController;
+    public reel2Script: reelController;
+    public reel3Script: reelController;
 
 
     start() {
@@ -42,14 +42,12 @@ export class board1Controller extends Component {
             this.reel2Script.startReel();
             this.reel3Script.startReel();
 
-            this.reel1Script.setResult([7,6,5,4,3,2,1]);
-            this.reel2Script.setResult([5,4,1,6,3,2,7]);
-            this.reel3Script.setResult([5,4,1,3,2,6,7]);
+            this.reel1Script.setResult(this.generateRandomArray());
+            this.reel2Script.setResult(this.generateRandomArray());
+            this.reel3Script.setResult(this.generateRandomArray());
 
         }
         else {
-    
-
             this.reel1Script.stopReel();
             this.reel2Script.stopReel();
             this.reel3Script.stopReel();
@@ -60,18 +58,18 @@ export class board1Controller extends Component {
 
     generateRandomArray() {
         let result: number[] = [];
-      
-        while (result.length < 3) {
-          const randomNumber = Math.floor(Math.random() * (7)) + 1;
-      
-          // Kiểm tra số ngẫu nhiên đã tồn tại trong mảng chưa
-          if (result.indexOf(randomNumber)==-1) {
-            result.push(randomNumber);
-          }
+
+        while (result.length < 7) {
+            const randomNumber = Math.floor(Math.random() * (7)) + 1;
+
+            // Kiểm tra số ngẫu nhiên đã tồn tại trong mảng chưa
+            if (result.indexOf(randomNumber) == -1) {
+                result.push(randomNumber);
+            }
         }
 
         return result;
-      }
+    }
 
     update(deltaTime: number) {
 
